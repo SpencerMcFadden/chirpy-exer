d81273683d00b696acef9c6ec5e1d3b8ee545558
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-09-08 10:17:27 -0400

    adds readiness handler. sets up base structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-function main() {
-  console.log("Hello, World!");
-}
+import express, { Express, Request, Response } from "express";
 
-main();
+const app: Express = express();
+const port = 8080;
+const handlerReadiness = (req: Request, res: Response) => {
+  res.set({
+    "Content-Type": "text/plain; charset=utf-8",
+  });
+  res.send("OK");
+};
+
+app.get("/healthz", handlerReadiness);
+app.use("/app", express.static("./src/app"));
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
