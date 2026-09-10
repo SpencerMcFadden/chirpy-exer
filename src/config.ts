f817057801d7aa2edac940d6f396dc0b8bf8a1bf
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-09-10 11:45:18 -0400

    adds drizzle config, db index file, and simple user query. updates ignore file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
+process.loadEnvFile();
+
 type APIConfig = {
+  dbURL: string;
   fileserverHits: number;
 };
 
 export const config: APIConfig = {
+  dbURL: envOrThrow("DB_URL"),
   fileserverHits: 0,
 };
+
+function envOrThrow(key: string) {
+  const env = process.env[key];
+  if (!env) {
+    throw new Error(`Environment variable ${key} failed to load`);
+  }
+  return env;
+}
---
+++ src/db/queries/users.ts
@@ -0,0 +1,7 @@
+import { db } from "../index.js";
+import { User, users } from "../schema.js";
+
+export async function createUser(user: User) {
+  const [result] = await db.insert(users).values(user).onConflictDoNothing().returning();
+  return result;
+}
